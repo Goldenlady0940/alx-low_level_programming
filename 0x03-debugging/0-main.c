f6b2cca543f61284d9271e18e5ec1de4173a053c
Goldenlady040 <sebrinaa18@gmail.com>
2022-06-08 09:09:37 -0700

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * positive_or_negative - checks the positivity and negativity of the number
+ * @i: number to be checked
+ */
+void positive_or_negative(int i)
+{
+	int i;
+
+	positive_or_negative(i);
+}
