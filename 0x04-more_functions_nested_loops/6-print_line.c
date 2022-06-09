702333ba388240918c6f8d47cf52d239afd2cdb9
Goldenlady0940 <104963156+Goldenlady0940@users.noreply.github.com>
2022-06-09 22:28:30 +0300

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,24 +1,20 @@
 #include "main.h"
 
 /**
- * print_line - prints a line in the terminal
- * @n: number of the_ lines to be printed
+ * print_line - prints a line of _ n long
+ * @n: length of line
+ *
+ * Return: void
  */
 
 void print_line(int n)
 {
-	int i;
+	int line = 0;
 
-	if (n <= 0)
+	while (line < n)
 	{
-		_putchar('\n');
+		_putchar('_');
+		line++;
 	}
-	else
-	{
-		for (i == 0; i < n; i++)
-		{
-			_putchar('_');
-		}
-		_putchar('\n');
-	}
+	_putchar('\n');
 }
