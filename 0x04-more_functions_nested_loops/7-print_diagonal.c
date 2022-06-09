dce5c38b48f56e1e121c44860a7208ec3ae97173
Goldenlady0940 <104963156+Goldenlady0940@users.noreply.github.com>
2022-06-09 22:42:29 +0300

    task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,16 +3,13 @@
 /**
  * print_diagonal - diagonal line
  @n: size for the diagonal
- 
  * Return: void
  */
 
 void print_diagonal(int n)
 {
-	char a;
-
-	char b;
-
+	char a, b;\
+		
 	if (n > 0)
 	{
 		for (a = 1; a <= n; a++)
