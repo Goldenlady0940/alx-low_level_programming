f095b8c30ab9fc6c18d4446b1ce0365dbfa0f34e
Goldenlady040 <sebrinaa18@gmail.com>
2022-06-09 09:40:55 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,22 +2,20 @@
 
 /**
  * print_most_numbers - print numbers 0-9 except 2 n 4
+ *
+ * Return: void
  */
 
 void print_most_numbers(void)
 {
 	int i;
 
-	for( i = 48; i < 58; i++)
+	for( i = '0'; i < '9'; i++)
 	{
-		if (i == 50 || i == 52)
-		{
+		if (i == '2' || i == '4')
 			continue;
-		}
 		else
-		{
 			_putchar(i);
-		}
 	}
 	_putchar('\n');
 }
