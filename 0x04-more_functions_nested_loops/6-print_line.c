d402108febaafd4e8ceff805a15f5f024d636a90
Goldenlady040 <sebrinaa18@gmail.com>
2022-06-09 06:14:33 -0700

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,15 +9,15 @@ void print_line(int n)
 {
 	int i;
 
-	for (i == 0; i <= n; i++)
+	if (n <= 0)
 	{
-		if (n <= 0)
-		{
 		_putchar('\n');
-		}
-		else
+	}
+	else
+	{
+		for (i == 0; i <= n; i++)
 		{
-		_putchar("_");
+			_putchar("_");
 		}
 	}
 	_putchar('\n');
