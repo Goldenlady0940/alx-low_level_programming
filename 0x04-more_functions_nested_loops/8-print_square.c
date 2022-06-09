2a4a5a2eaa31ba52d082f641cc6982f658d900f2
Goldenlady040 <sebrinaa18@gmail.com>
2022-06-09 08:34:30 -0700

    task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -13,9 +13,9 @@ void print_square(int size)
 
 	if (size > 0)
 	{
-		for (i == 0; i < size; i++)
+		for (i == '0'; i < size; i++)
 		{ 
-			for (j == 1; j < i; j++)
+			for (j == '1'; j < i; j++)
 			{
 				_putchar('#');
 			}
