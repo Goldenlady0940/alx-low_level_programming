53c29224224a981cde41e154781d90e38a4f28a1
Goldenlady040 <sebrinaa18@gmail.com>
2022-06-09 05:57:15 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,14 +6,14 @@
 
 void more_numbers(void)
 {
-	int i;
+	int i, j, k;
 
 	for (i = 48; i <= 62; i++)
 	{
 		if (i >= 58)
 		{
-			int j = i / 10 + '0';
-			int k = i % 10 + '0';
+			j = i / 10 + '0';
+			k = i % 10 + '0';
 			_putchar(j);
 			_putchar(k);
 		}
