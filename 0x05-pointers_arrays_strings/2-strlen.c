f0c7f409689bf12c1f07ee48cf10ca4ebced1e2e
Goldenlady0940 <sebrinaa18@gmail.com>
2022-06-13 17:42:17 +0300

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+include "main.h"
+
+/**
+ * _strlen - computes length of a string
+ * @s: string
+ *
+ * Return: 0 on Sucess
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(*s);
+	return (len);
+}
