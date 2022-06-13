cb7f330f598fb155c679a74b2548d3f9c7d68228
Goldenlady0940 <sebrinaa18@gmail.com>
2022-06-13 23:32:46 +0300

    task 11

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,34 +1,21 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
- * *_strcpy - copies the string pointed to the src including the delimeter
- * @dest: copy destination
- * @src: copy source
- *
- * Return: char
-=======
- * _strcpy - copies the string pointed to by source to dest
- * @src: source
+ * _strcpy - copies the string pointed to by src into dest
  * @dest: destination
- *
- * Return : char
->>>>>>> f86d9aebb4defae7ae241047dc2b898252a1527d
+ * @src: source
+ * Return: char with copy of string
  */
 
 char *_strcpy(char *dest, char *src)
 {
-<<<<<<< HEAD
-
-=======
 	int i;
 
-	for (i = 0; src[i] != '\0'; i++)
+	for (i = 0; *(src + i) != '\0'; i++)
 	{
-		dest[i] = src[i];
+		dest[i] = *(src + i);
 	}
 	dest[i] = '\0';
 
 	return (dest);
 }
->>>>>>> f86d9aebb4defae7ae241047dc2b898252a1527d
