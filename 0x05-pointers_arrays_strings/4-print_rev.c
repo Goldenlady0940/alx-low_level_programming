e21263ab365225f0ddba491fdc6fe231998478c0
Goldenlady0940 <sebrinaa18@gmail.com>
2022-06-13 18:08:55 +0300

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev: prints reverse of a string
+ * @s: string to be reversed
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	char str[];
+
+	int i;
+
+	for (i = strlen(s); i >= 0; i--)
+	{
+		str[i] = s[i];
+	}
+	puts(str);
+}
