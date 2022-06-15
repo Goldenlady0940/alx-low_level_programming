6dd2137a3de8704e631411057be6bf8fbcf63274
Goldenlady0940 <sebrinaa18@gmail.com>
2022-06-16 01:44:55 +0300

    task 10

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <string.h>
 
-int _putchar(char i);
+int _putchar(char );
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
