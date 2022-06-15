4085622871563414a033e7e0dfaf7e8bc85e63e3
Goldenlady0940 <sebrinaa18@gmail.com>
2022-06-16 00:13:46 +0300

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes lower to upper
+ * @c: letter to be changed
+ *
+ * Return: char
+ */
+
+char *string_toupper(char *c)
+{
+	int i;
+
+	for (i = 0; i <= strlen(c); i++)
+	{
+		if (c >= 'a' && c <= 'z')
+		{
+			c += 32;
+		}
+	}
+	return (c);
+}
