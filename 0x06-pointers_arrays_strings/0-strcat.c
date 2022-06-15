372d45ba4e8f3e434618534111ac0b3bed238ee8
Goldenlady0940 <sebrinaa18@gmail.com>
2022-06-15 22:10:20 +0300

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcat - appends the source with destination
+ * @dest: deestination
+ * @src: source
+ *
+ * Return: char
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = dest[i] + src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
