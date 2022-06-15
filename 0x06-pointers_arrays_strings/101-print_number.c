908de033153d8fdd474d1204a8e3cf0e86841a39
Goldenlady0940 <sebrinaa18@gmail.com>
2022-06-16 01:37:14 +0300

    task 10

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_number - print an integer
+ * @n: int to be printed
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	_putchar(n);
+}
---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * infinite_add - adds two numbers
+ * @n1: 1st number
+ * @n2: 2nd number
+ * @r: saves result
+ * @size_r: buffer size
+ *
+ * Return: char
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	r = n1 + n2;
+	return (r);
+}
