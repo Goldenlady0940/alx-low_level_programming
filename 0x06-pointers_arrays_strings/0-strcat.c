62e6d453f025cde73641025464c1fa6ec6ec69a2
Goldenlady0940 <sebrinaa18@gmail.com>
2022-06-16 16:49:09 +0300

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,9 +12,10 @@ char *_strcat(char *dest, char *src)
 {
 	int i;
 
-	for (i = 0; src[i] != '\0'; i++)
+	for (i = 0; src[i] = '\0'; i++)
 	{
-		dest[i] = dest[i] + src[i];
+		if (src[i] == '\0')
+			dest[i] = dest[i] + src[i];
 	}
 	dest[i] = '\0';
 	return (dest);
