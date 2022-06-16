0c8cf1f84b63fb88f4c559244f69259b0f6ff1bc
Goldenlady0940 <104963156+Goldenlady0940@users.noreply.github.com>
2022-06-16 20:06:24 +0300

    task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,12 +10,15 @@
 
 void reverse_array(int *a, int n)
 {
-	int temp, i;
+	int temp, i, j;
 
 	for (i = 0; i < n - 1; i++)
 	{
-		temp = a[i];
-		a[i] = a[n - i];
-		a[n - i] = temp;
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = a[i];
+			a[i] = a[n - i];
+			a[n - i] = temp;
+		}
 	}
 }
