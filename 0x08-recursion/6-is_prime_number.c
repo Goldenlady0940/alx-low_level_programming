be95f3a6f322950a6537c11c28f95dc2c4962cdc
Goldenlady0940 <104963156+Goldenlady0940@users.noreply.github.com>
2022-06-22 22:16:35 +0300

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,29 +1,28 @@
 #include "main.h"
-
 /**
- * is_prime_number - checks if its prime number
- * @n: number to be checked
+ * _sqrt_recursion - a function that returns the square root of a number
  *
- * Return: 1 on success
+ * @n: the passed value
+ *
+ * Return: if no sqrt, -1. Else the sqrt.
  */
-
-int ispal(char *s, int i, int j)
+int sqrt(int l, int h, int n)
 {
+	int m = (l + h) / 2;
 
-    if (s == j)
-        return 1;
-    if (s[i] != s[j])
-        return 0;
-    if (i < j + 1)
-        return (ispal(s, i + 1, j - 1));
-    return 1;
+	if ((m * m <= n) && ((m + 1) * (m + 1) > n))
+		return (m);
+	else if (mid * mid > n)
+		return sqrt (m + 1, h, n);
+	else
+		return sqrt (l, m - 1, n);
 }
-int is_palindrome(char *s)
+int _sqrt_recursion(int n)
 {
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+	return (sqrt (0, n, n));
 
-    int n = strlen(s);
-    if (n != 0)
-    {
-        return ispal(s, 0, n - 1);
-    }
 }
