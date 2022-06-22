c8e5121ca76069de60784e9c6b059bd0508accc0
Goldenlady0940 <104963156+Goldenlady0940@users.noreply.github.com>
2022-06-22 22:14:34 +0300

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,6 +7,23 @@
  * Return: 1 on success
  */
 
-int is_prime_number(int n)
+int ispal(char *s, int i, int j)
 {
 
+    if (s == j)
+        return 1;
+    if (s[i] != s[j])
+        return 0;
+    if (i < j + 1)
+        return (ispal(s, i + 1, j - 1));
+    return 1;
+}
+int is_palindrome(char *s)
+{
+
+    int n = strlen(s);
+    if (n != 0)
+    {
+        return ispal(s, 0, n - 1);
+    }
+}
