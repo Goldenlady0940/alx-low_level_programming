c804683345fc6e40a5e5094c4b454e20d7522936
Goldenlady0940 <sebrinaa18@gmail.com>
2022-06-22 22:20:40 +0300

    task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,7 +1,10 @@
 #include "main.h"
 
 /**
- * is_palindrome - checks palindrome
+ * is_palindrome - checks palindrom
+ * ispal - checks if equal
+ * @i: row
+ * @j: column
  * @s: number to be checked
  *
  * Return: 1 if pal and 0 if not
@@ -10,12 +13,21 @@
 int ispal(char *s, int i, int j)
 {
 	if (*s == j)
+	{
 		return 1;
+	}
 	if (s[i] != s[j])
+	{
 		return 0;
+	}
 	if (i < j + 1)
+	{
 		return (ispal(s, i + 1, j - 1));
-	return 1;
+	}
+	else
+	{
+		return 1;
+	}
 }
 
 int is_palindrome(char *s)
@@ -24,6 +36,8 @@ int is_palindrome(char *s)
 
 	n = strlen(s);
 	if (n != 0)
+	{
 		return ispal(s, 0, n - 1);
+	}
 	return ('\n');
 }
