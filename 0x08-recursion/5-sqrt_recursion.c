fd1b38afda9c755a7046965a7d8aed171f578443
Goldenlady0940 <104963156+Goldenlady0940@users.noreply.github.com>
2022-06-22 22:25:36 +0300

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,12 +1,28 @@
-#include "main.h"
-
+include "main.h"
 /**
- * _sqrt_recursion - calculates sqrt of a number
- * @n: number to be in root
+ * _sqrt_recursion - a function that returns the square root of a number
  *
- * Return: root which is an int
+ * @n: the passed value
+ *
+ * Return: if no sqrt, -1. Else the sqrt.
  */
+int sqrt(int l, int h, int n)
+{
+	int m = (l + h) / 2;
 
+	if ((m * m <= n) && ((m + 1) * (m + 1) > n))
+		return (m);
+	else if (mid * mid > n)
+		return sqrt (m + 1, h, n);
+	else
+		return sqrt (l, m - 1, n);
+}
 int _sqrt_recursion(int n)
 {
-	if (
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+	return (sqrt (0, n, n));
+
+}
