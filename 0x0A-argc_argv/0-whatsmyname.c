6c360eda6b83ad183163987ceda01fcba9527c25
Goldenlady0940 <sebrinaa18@gmail.com>
2022-06-24 19:37:09 +0300

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - the function to print the name of a program
+ * @argc: the count
+ * @argv: the array
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
