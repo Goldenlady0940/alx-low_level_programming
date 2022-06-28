e6d237ed7e4bbad2ea8f626b163208881bf56782
Goldenlady0940 <104963156+Goldenlady0940@users.noreply.github.com>
2022-06-28 21:01:54 +0300

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,33 +1,43 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
- * str_concat - a function that concatenates two strings
- * @s1: first passed string
- * @s2: second passed string
- * Return: the concatenated string or null
+ * str_concat - concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
+ *
+ * Return: pointer of an array of chars
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *con;
-	int i;
-	int a;
-	int b;
+	char *strout;
+	unsigned int i, j, k, limit;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (a = 0; s1[a] != '\0'; a++);
-	for (b = 0; s2[b] != '\0'; b++);
-	b += 1;
-	a += b;
-	con = malloc (a * sizeof(char));
-	if (con == NULL)
+
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+
+	strout = malloc(sizeof(char) * (i + j + 1));
+
+	if (strout == NULL)
+	{
+		free(strout);
 		return (NULL);
-	for (a = 0: s1[a] != '\0'; a++)
-		con[a] = s1[a];
-	for (b = 0; s2[b] != '\0'; a++, b++)
-		con[a] = s2[b];
-	con[a] = '\0';
-	return (con);
+	}
+
+	for (k = 0; k < i; k++)
+		strout[k] = s1[k];
+
+	limit = j;
+	for (j = 0; j <= limit; k++, j++)
+		strout[k] = s2[j];
+
+	return (strout);
 }
