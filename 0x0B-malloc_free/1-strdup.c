7379b5b8aace854b8e707e524b0bf27dc22267fa
Goldenlady0940 <104963156+Goldenlady0940@users.noreply.github.com>
2022-06-28 21:00:06 +0300

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,24 +1,28 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
-
 /**
- * _strdup - holds pointer pointing to a memory having copy of the parameter
- * @str: the strint parameter
+ * _strdup - returns a pointer to a newly allocated space in memory.
+ * @str: string.
  *
- * Return: char pointer
+ * Return: pointer of an array of chars
  */
 char *_strdup(char *str)
 {
-	char *p;
-	unsigned int i;
+	char *strout;
+	unsigned int i, j;
 
 	if (str == NULL)
 		return (NULL);
-	p = malloc(sizeof(str) * 3);
-	if (p == NULL)
+
+	for (i = 0; str[i] != '\0'; i++);
+
+	strout = (char *)malloc(sizeof(char) * (i + 1));
+
+	if (strout == NULL)
 		return (NULL);
-	for (i = 0; str[i] != '\0'; i++)
-		p[i] = str[i];
-	return (p);
+
+	for (j = 0; j <= i; j++)
+		strout[j] = str[j];
+
+	return (strout);
 }
