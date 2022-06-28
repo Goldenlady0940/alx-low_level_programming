b6da95e3b869e7b5e9098ede654b7ea326cd9f43
Goldenlady0940 <104963156+Goldenlady0940@users.noreply.github.com>
2022-06-28 20:34:47 +0300

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,16 +14,22 @@ int **alloc_grid(int width, int height)
 	int i, j;
 	int **grid;
 
-	if ( width < 1 || height < 1)
+	if (width < 1 || height < 1)
 		return (NULL);
 	grid = malloc(sizeof(int *) * height);
 	if (grid == NULL)
+	{
+		free(grid);
 		return (NULL);
+	}
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = malloc(sizeof(int) * width);
 		if (grid[i] == NULL)
+		{
+			free(grid);
 			return (NULL);
+		}
 	}
 	for (i = 0; i < height; i++)
 	{
