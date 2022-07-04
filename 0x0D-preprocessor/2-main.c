e947defab6c53093c514067733329df8c17a7152
Goldenlady0940 <sebrinaa18@gmail.com>
2022-07-04 22:55:40 +0300

    tasks

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * main - print source file
+ *
+ * Return: zero.
+ */
+int main(void)
+{
+	printf(__BASE_FILE__);
+	printf("\n");
+	return (0);
+}
