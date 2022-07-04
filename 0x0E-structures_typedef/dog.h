80f7eaf6df6ab50eff6d9d52c5b4bdbe2c5dd151
Goldenlady0940 <sebrinaa18@gmail.com>
2022-07-04 22:00:00 +0300

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+/**
+ * struct dog - structure for dog
+ * @name: 1st attribute
+ * @age: 2nd attribute 
+ * @owner: 3rd attribute
+ */
 struct dog
 {
 	char* name;
 	float age;
 	char* owner;
-}
-
+};
+#endif
